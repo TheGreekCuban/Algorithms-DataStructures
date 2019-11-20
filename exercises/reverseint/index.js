7493bc1fd2862f83d2acfc3d318d37418b210b36
TheGreekCuban <theodorealexander92@gmaill.com>
2019-11-19 19:20:28 -0500

    learned how to reverse an int using Math.sign() and parsefloat

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+//const reverseInt = num => parseFloat(num.toString().split('').reverse().join('')) * Math.sign(num)
+
+/*This function is easier to read if you want to understand what is goign on. We save the number, turned into a string, split into an array of strings, reversed and joined back together to a variable named REVERSE. Then, we return the string turned into a number using parseFloat and multiplying that by Math.sign(num). If num is negative (Math.sign(num) = -1) which turns the returned number negative. Parse float eliminates anything that is not a number which is why the "-" doesn't come through. 
+*/
+
+function reverseInt(num) {
+    let reverse = num.toString().split('').reverse().join('')
+      
+    return parseFloat(reverse) * Math.sign(num)                
+  }
+
+console.log(reverseInt(-14579))
 
 module.exports = reverseInt;
