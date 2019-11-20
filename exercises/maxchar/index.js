c1f5ff171e0cc1edaaafb172f0af0d05a69b3df5
TheGreekCuban <theodorealexander92@gmaill.com>
2019-11-19 20:45:36 -0500

    learned ot map out array and then compare its values to return the key name with the largest value

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,32 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+//THIS PROBLEMS TECHNIQUE CAN BE USED TO SOLVE THE FOLLOWING PROBLEM TYPES:
+//1. WHAT IS THE MOST COMMON CHARACTER IN THE STRING?
+//2. DOES STRING A HAVE THE SAME CHARACTERS AS STRING B (IS IT AN ANAGRAM)
+//3. DOES THE GIVEN STRING HAVE REPEATED CHARACTERS IN IT?
+
+const maxChar = str => {
+    /*
+    First we take the string, split it into an array of each character. After that we run reduce with the initial value as an empty object. We use the in operator to check if the letter is already a key in the object. If it is, we increment the number in by grabbing the value with object[letter]. If it isn't, that means it's the first time that character shows up, so set object[letter] = 1. Once all is iterated through, return the object containing the character as a key and the amount of time it appears as the keys value.
+    */
+    let strObj = str.split("").reduce((object, letter) => {
+        if(letter in object) {
+            object[letter]++
+        } else {
+            object[letter] = 1
+        }
+        return object
+    }, {})
+
+    console.log("strObj keys: ", Object.keys(strObj))
+    /*
+    Then we want to return the key of the highest valye in that newly created object. We do this using Object.keys on the strObj. Then we run reduce on that value. Simply put, this function in reduce does not have an initial value so the accumulator is set to the first element in the object. In the reduce we are comparing the values of each key in the object. If the accumulator val is > than the next element, we return the accumulator. This happens until every key is iterated over. We are left returning the largest valued object key.
+    */
+
+    return Object.keys(strObj).reduce((a, b) => strObj[a] > strObj[b] ? a : b);
+}
+
+console.log(maxChar("abcccccccddd"))
 
 module.exports = maxChar;
