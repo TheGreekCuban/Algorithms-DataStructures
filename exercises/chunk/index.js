180f37af7902148c173ed841600470c2c9a1e4ea
TheGreekCuban <theodorealexander92@gmaill.com>
2019-11-23 15:33:35 -0500

    chunk solution 1

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,19 +8,34 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-const chunk = (array, size) => {
-  let chunked = [] 
-  let innerArray = []
 
-  array.forEach(element => {
-    if(!chunked.length-1 || innerArray.length !== size) {
-      innerArray.push(element)
-    } else {
-      chunked.push(innerArray)
-      innerArray = []
-    }
+
+
+
+/*
+1. Create an empty array called chunked that will hold the arrays being split up
+2. For each element in the unchunked array:
+  A. Retrieve the last element in the chunked array
+  B. If the last element does not exist, or if the last element in the chunked array === chunk size
+    a. Push a new chunk into the chunked array with the current element. 
+  C. Else add the current element into the chunk
+*/
+const chunk = (array, size) => {
+  let chunked = [];
+  
+  //Cleaner For Loop Syntax
+  for(let element of array) {
+    //Grab the last element of the chunked array which is going to be returned
+    let last = chunked[chunked.length-1]
     
-  })
+    //If there is no last element, we need to push a chunk in. OR if there is a last element but it's equal to chunk size, we need to start a new chunk. This if statement takes care of identifying if a new chunk needs to be added and adding the first element into that chunk.
+    if(!last || last.length === size) {
+      chunked.push([element])
+    } else {
+      //If there is a last element but it's not full then we just push element into last
+      last.push(element)
+    }
+  }
   return chunked
 }
 
