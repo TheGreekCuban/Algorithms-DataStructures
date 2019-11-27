dcbbef2223b4d2d0950f8911d85267bc5f1a210f
TheGreekCuban <theodorealexander92@gmaill.com>
2019-11-26 22:01:33 -0500

    solution 2 anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,9 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+
+//SOLUTION 1: MY SOLUTION
+/*
 const anagrams = (stringA, stringB) => {
     //WE ARE USING A REGEX EXPRESSION TO STRIP THE WORDS OF ALL SPACES AND CHARACTERS OTHER THAN LETTERS, THEN PUT THEM AS A LOWER CASE WORD IN A VARIABLE.
     let strA = stringA.replace(/[^\w]/g, "").toLowerCase().split("")
@@ -35,15 +38,50 @@ const anagrams = (stringA, stringB) => {
             }
             return object
         },{})
-
-        console.log("strObjA: ", strObjA, "strObjB: ", strObjB)
- 
+      //NEED TO RUN REDUCE COMPARING ALL ELEMENTS IN BOTH CHARACTER MAPS. IF ALL KEY VALUE PAIRS MATCH, RETURN TRUE. 
       return Object.keys(strObjA).reduce((a, b) => strObjA[a] === strObjB[a] && strObjA[b] === strObjB[b] ? true : false)
     } else {
         return false 
-    }
-        
+    }    
 }
+*/
+
+//=============================================//
+
+//SOLUTION 2: SIMILAR SOLUTION
+const anagrams = (stringA, stringB) => {
+    let aCharMap = buildCharMap(stringA)
+    let bCharMap = buildCharMap(stringB)
+
+    //EDGE CASE: If the length of the object.keys of both charMaps are not equal then it cannot be an anagram so return false.
+    if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+        return false
+    } 
+        
+    //We will loop through the object charMap and compare it with the other. Objects use for in not for of
+    for(let char in aCharMap) {
+      //if the value key pair of charMapA is not = to a key value pair of charMapB when given the same key, its not an anagram
+      if(aCharMap[char] !== bCharMap[char]) {
+        return false
+      }
+    }
+    //since we check for all cases of it possibly being false, we return true. 
+    return true
+}
+
+//IMPORTANT: I COULD HAVE CREATED A HELPER FUNCTION TO TAKE CARE OF THE TWO STEPS IN CREATING THE CHARACTER MAP. THIS WOULD BE MUCH CLEANER AND FAR MORE PREFERABLE TO EMPLOYERS.
+const buildCharMap = (str) => {
+    charMap = {}
+    
+    //WE RUN A LOOP ON A SPACELESS, SPECIAL CHARACTER-LESS STRING B/C OF REGEX AND ALSO ALL LOWER CASE
+    for(let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+        //In the loop we are either adding 1 to every instance where a key is iterated over multiple times or we are setting the first instance of that key to one. 
+        charMap[char] = charMap[char] + 1 || 1
+    }
+
+    return charMap
+}
+
 
 console.log("return: ", anagrams('A tree, a life, a bench', 'A tree, a fence, a yard'))
 
