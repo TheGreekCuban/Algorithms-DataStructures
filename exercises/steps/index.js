cb56a7a137941ee098a63aae18631cd1c2ad9830
TheGreekCuban <theodorealexander92@gmaill.com>
2020-02-03 20:03:57 -0500

    recursion

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -18,9 +18,8 @@
 //       '####'
 
 /*
+    THE BEST WAY TO ATTACK THIS WAS TO DRAW A DIAGRAM OF THE MATRIX CREATED SO YOU COULD MARK UP EACH POSTITION OF EACH ELEMENT IN THE MATRIX. YOU WOULD NOTICE THAT ONCE THE ROW IDX IS GREATER THAN OR EQUAL TOO THE COLUMN IDX WE CAN CONCATENATE #S. ONCE THAT CONDITION IS NOT MET, WE START ADDING SPACES AND CONSOLE.LOG THE STRING ONCE THE LENGTH OF THE STRING IS === N.
 
-        THE BEST WAY TO ATTACK THIS WAS TO DRAW A DIAGRAM OF THE MATRIX CREATED SO YOU COULD MARK UP EACH POSTITION OF EACH ELEMENT IN THE MATRIX. YOU WOULD NOTICE THAT ONCE THE ROW IDX IS GREATER THAN OR EQUAL TOO THE COLUMN IDX WE CAN CONCATENATE #S. ONCE THAT CONDITION IS NOT MET, WE START ADDING SPACES AND CONSOLE.LOG THE STRING ONCE THE LENGTH OF THE STRING IS === N.
-*/
 
 const steps = n => {
 
@@ -31,8 +30,8 @@ const steps = n => {
 
         //ITERATE THROUGH THE COLUMNS
         for(let column = 0; column < n; column++) {
-            //IF CURRENT COLUMN IS >= ROW WE CAN += "#" ELSE ADD A SPACE
-            //This needs to be <= because if both column and row are 0, they are equal. However, that condition is not met and then the first stair that is logged is a totally empty string who's length is = to n. Dont forget the = in the comparison operator.
+            //IF CURRENT COLUMN IS <= ROW WE CAN += "#" ELSE ADD A SPACE
+            //This needs to be <= because if both column and row are 0, they are equal. However, the < condition is not met and then the first stair that is logged is a totally empty string who's length is = to n. Dont forget the = in the comparison operator.
             column <= row ? stair += "#" : stair += " "
         }
 
@@ -42,6 +41,49 @@ const steps = n => {
         }
     }
 
+}
+*/
+
+//RECURSION SOLUTION
+//WITH RECURSION WE ALWAYS START OUT WITH A BASE CASE -> THIS IS THE CONDITION THAT NEEDS TO BE MET TO STOP THE RECURSION PROCESS.
+//AFTER DEFINING BASE CASE WE WANT TO DO SOME AMOUNT OF WORK AND THEN CALL THE FUNCTION AGAIN, WE MUST MAKE SURE TO CHANGE THE ARGUMENT SOMEHOW.
+
+//TIPS
+//TRY AND FIGURE OUT THE BEAR MINIMUM PIECES OF INFORMATION TO REPRESENT YOUR PROBLEM
+//GIVE REASONABLE DEFAULTS TO THOSE PIECES OF MINIMUM INFORMATION (SEE ES6 DEFAULT VALUES)
+//CHECK THE BASE CASE, IS THERE ANY MORE WORK TO DO? IF NOT, RETURN.
+//DO SOME WORK, CALL YOUR FUNCTION AGAIN, MAKING SURE THE ARGUMENTS HAVE CHANGED IN SOME FASHION. CONSOLE.LOG(ARGUMENTS)
+
+/*
+    RECURSION BREAKDOWN -> INVISION MATRIX LIKE ABOVE
+
+    1. IF (ROW === N) THEN WE HAVE HIT THE END OF OUR PROBLEM
+    2. IF THE STAIR STRING HAS A LENGTH === N THEN WE ARE AT THE END OF A ROW
+    IF THE LENGTH OF THE STAIR STRING IS LESS THAN OR EQUAL TO THE ROWN NUMBER WERE WORKING ON, WE ADD A # OTHERWISE WE ADD A SPACE. 
+*/
+
+//We add row as an argument with a default value of 0 because that is what the starting index row would be in our recursive solution because we will need to continually pass that data down the stack so we can compare it to our base case. 
+const steps = (n, row = 0, stair = "") => {
+
+    if (row === n) {
+        return;
+    }
+
+    //Stair is another variable we need to keep track of so we make it an argument witha default value of an empty string
+    if (n === stair.length) {
+        console.log(stair);
+        //We don't need to pass in an empty string because we set the default value of an empty string in the initial function definition. Just need to increment row so that it continues going up.
+        return steps(n, row + 1)
+    }
+
+    if (stair.length <= row) {
+        stair += "#";
+    } else {
+        stair += " ";
+    }
+
+    //This is very important because we need to recurse THE COLUMNS AS WELL. Right now we are recursing through rows but not horizontally. So we call steps again, pass in row (without changing the value because that is provided by the level above in the stack) and pass in stair, which is the current status of the string. 
+    steps(n, row, stair);
 }
 
 steps(4)
