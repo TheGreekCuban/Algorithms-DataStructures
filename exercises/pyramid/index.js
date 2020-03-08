9fad540aab058b2a33d0339c4a812d36a5051406
TheGreekCuban <theodorealexander92@gmaill.com>
2020-03-07 22:00:43 -0500

    hellop

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,29 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+const pyramid = n => {
+    //We save the midpoint vairable at the top of the function as it is something we can use throughtout the function.
+    const midpoint = Math.floor((2 * n - 1) / 2)
+    
+    //Just as in the other problem we need a loop to interate the row
+    for(let row = 0; row < n; row++) {
+        //The variable that this loop will concatenate and log every iteration.
+        let level = ""
+
+        //We must iterate through the colums. This time the relationship between n and the #'s in columns is 2n - 1 so the loop has to stop before that number.
+        for(let column = 0; column < 2 * n - 1; column++) {
+
+            //If you notice the relationship on paper, the # are always printed within a range of the midpoint, this range is midpoint + or - the row number currently on. This is why on row 0, the only # printed is the midpoint because there is no range on either side. If the column is outside of this range, you simply concatenate a space!
+            midpoint - row <= column && midpoint + row >= column ? level += '#' : level += " "            
+        }
+        //Log the level every time the row iterates.
+        console.log(level)
+    }
+}
+
+
+
+pyramid(15)
 
 module.exports = pyramid;
+
